b980919b07801de3f72ef632415c1462429b3a68
MadhupriyaJ <madhu30071998@gmail.com>
2024-10-25 12:29:40 +0530

    modified .evn and topics.py

--- src/Components/Playbook/python/Topics.py
+++ src/Components/Playbook/python/Topics.py
@@ -5,31 +5,32 @@ from openai import AzureOpenAI
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv(dotenv_path='metronics_v8.2.0-main/src/Components/Playbook/python/.env')
+# load_dotenv(dotenv_path='metronics_v8.2.0-main/src/Components/Playbook/python/.env', override=True)
 
 
 
-azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-api_key = os.getenv("AZURE_OPENAI_API_KEY")
-print('azure_endpoint',azure_endpoint)
-print('api_key',api_key )
+# azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+# api_key = os.getenv("AZURE_OPENAI_API_KEY")
+# print('azure_endpoint',azure_endpoint)
+# print('api_key',api_key )
+
 
 # Initialize Azure OpenAI client
 client = AzureOpenAI(
-    azure_endpoint=azure_endpoint,
-    api_key=api_key,
-    api_version="2024-05-01-preview"
+    azure_endpoint='https://topicmodelingforthottaneketta.openai.azure.com/',
+    api_key='a279c736c8e64b3fa82f8f1958696cf6',
+    api_version="2024-02-15-preview"
 )
 
 # Create the assistant
 assistant = client.beta.assistants.create(
-    model="gpt-4o",  # Replace with model deployment name.
+    model="gpt-4",  # Replace with model deployment name.
     instructions=(
         "You are an expert in topic modeling and text analysis. "
         "Analyze the following text and identify the main topics. Provide a list of 1-3 topics without a  description for each. Text: {text}"
     ),
-    tools=None,
-    tool_resources={},
+    # tools=None,
+    # tool_resources={},
     temperature=1,
     top_p=1
 )
